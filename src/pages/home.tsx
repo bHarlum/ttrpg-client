b30fec01dba6f297bbc3ddfca9065afe95916b54
Bryce Harlum <bryce.harlum@gmail.com>
2020-08-16 11:31:19 +1000

    added react redux

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -9,6 +9,7 @@ export default function HomePage() {
     {i: 'b', x: 1, y: 0, w: 3, h: 2, minW: 2, maxW: 4},
     {i: 'c', x: 4, y: 0, w: 1, h: 2}
   ];
+  
   return (
     <StyledGridContainer>
       <GridLayout className="layout" layout={layout} cols={12} rowHeight={30} width={1200}>
