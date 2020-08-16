c6e6fd14f3391a1d5b38cfd2075822ddd2a8f665
Bryce Harlum <bryce.harlum@gmail.com>
2020-08-16 11:14:50 +1000

    initial setup of router and added types

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,28 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import {
+  BrowserRouter,
+  Switch,
+  Route,
+  Link,
+} from "react-router-dom";
+import HomePage from './pages/home';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <BrowserRouter>
+      <nav>
+        <li>
+          <Link to="/">
+            Home
+          </Link>
+        </li>
+      </nav>
+        <Switch>
+          <Route path="/" component={HomePage}/>
+        </Switch>
+      </BrowserRouter>
     </div>
   );
 }
